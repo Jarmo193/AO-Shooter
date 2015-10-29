80b41d1ebf32cb009f2d9ce2f0157470839647fe
unknown <RikuA@KOLAT-11-01.saiedu.fi>
2015-10-29 12:46:37 +0200

    Bullets baseclass

---
+++ Shooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+	public int Speed = 5;
+	public int Damage = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	protected void doDamage(GameObject target)
+	{
+
+	}
+}
