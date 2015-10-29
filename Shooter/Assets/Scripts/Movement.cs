2f6bac679f6233a216705b00d546a35034227ebe
unknown <jarnor@KOLAT-11-02.saiedu.fi>
2015-10-29 12:35:07 +0200

    Started

---
+++ Shooter/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float smooth;
+
+	private Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 newPos = Vector3.Lerp(rb.transform.position, targetPos, smooth);
+		rb.MovePosition(newPos);
+	}
+}
