df4a4a2f31aae49d72f4d76ae62ea43b83e443a9
unknown <jarnor@KOLAT-11-02.saiedu.fi>
2015-10-30 11:08:36 +0200

    EnemySpawner

---
+++ Shooter/Assets/Scripts/EnemySpawn/SpawnManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	public Object enemyShip;
+
+	private float maxWidth = 6.3f;
+	private float SpawnHeight = 7.95f;
+
+	float RandomX ()
+	{
+		return 0f;
+	}
+}
